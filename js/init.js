ecfd03a86c6178dcb346bbfa61ec953dd953d856
formair3 <139526331+formair3@users.noreply.github.com>
2023-11-16 16:42:19 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -9,11 +9,11 @@
 // @ts-ignore
 firebase.initializeApp({
     apiKey: "AIzaSyCLt-pvKk4FPUjo52baVBNQ5G96RcJgvXg",
-    authDomain: "nuevo-924f9.firebaseapp.com",
-    projectId: "nuevo-924f9",
-    storageBucket: "nuevo-924f9.appspot.com",
-    messagingSenderId: "649258399518",
-    appId: "1:649258399518:web:c774782a0f0f646e29fb0f",
-    measurementId: "G-LYVM1HV0TZ"
+  authDomain: "nuevo-924f9.firebaseapp.com",
+  projectId: "nuevo-924f9",
+  storageBucket: "nuevo-924f9.appspot.com",
+  messagingSenderId: "649258399518",
+  appId: "1:649258399518:web:c774782a0f0f646e29fb0f",
+  measurementId: "G-LYVM1HV0TZ"
 });
 
